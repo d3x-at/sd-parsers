2a0f6e5ce78d671e1edeaf9c81991b868d9b6cc4
d3x-at <70750382+d3x-at@users.noreply.github.com>
2023-04-18 18:31:54 +0200

    NovelAI: add model information, sanity checks

--- src/sdparsers/parsers/novelai.py
+++ src/sdparsers/parsers/novelai.py
@@ -1,6 +1,8 @@
 import json
+import re
+
 from ..parser import Parser
-from ..prompt_info import Prompt, PromptInfo, Sampler
+from ..prompt_info import Model, Prompt, PromptInfo, Sampler
 
 SAMPLER_PARAMS_DEFAULT = ["seed", "strength", "noise", "scale"]
 
@@ -10,26 +12,33 @@ class NovelAIParser(Parser):
 
     def __init__(self, config=None, process_items=True):
         super().__init__(config, process_items)
+        self._re_model = re.compile(r'^(.*?)\s+([A-Z0-9]+)$')
         self.sampler_params = self.config.get("sampler_params", SAMPLER_PARAMS_DEFAULT)
 
     def parse(self, image):
         params_comment = image.info.get('Comment')
         params_description = image.info.get('Description')
+        params_software = image.info.get('Software')
+        params_source = image.info.get('Source')
 
-        if params_comment is None or params_description is None:
+        if params_software != "NovelAI" \
+                or params_comment is None \
+                or params_description is None \
+                or params_source is None:
             return None
 
-        metadata = self._prepare_metadata(params_comment, params_description)
-        if metadata is None:
-            return None
+        metadata = self._prepare_metadata(
+            params_comment, params_description, params_source)
 
         return PromptInfo(generator=self.GENERATOR_ID, **metadata,
                           raw_params={
                               'Comment': params_comment,
-                              'Description': params_description
+                              'Description': params_description,
+                              'Software': params_software,
+                              'Source': params_source
                           })
 
-    def _prepare_metadata(self, params_comment, params_description):
+    def _prepare_metadata(self, params_comment, params_description, params_source):
         metadata = json.loads(params_comment)
 
         # prompt
@@ -39,6 +48,14 @@ class NovelAIParser(Parser):
             Prompt(negative_prompt_value) if negative_prompt_value else None
         )
 
+        # model
+        models = []
+        match = self._re_model.fullmatch(params_source)
+        if match:
+            model_name, model_hash = match.groups()
+            model = Model(model_name, model_hash)
+            models.append(model)
+
         # sampler
         sampler_parameters = []
         for key in self.sampler_params:
@@ -46,18 +63,17 @@ class NovelAIParser(Parser):
             if param:
                 sampler_parameters.append((key, param))
 
-        if not sampler_parameters:
-            return None
-
+        samplers = []
         try:
             sampler = Sampler(name=metadata.pop("sampler"),
                               parameters=self._process_metadata(sampler_parameters))
+            samplers.append(sampler)
         except KeyError:
-            return None
+            pass
 
         return {
             "prompts": [prompt],
-            "samplers": [sampler],
-            "models": [],
+            "samplers": samplers,
+            "models": models,
             "metadata": self._process_metadata(metadata)
         }
