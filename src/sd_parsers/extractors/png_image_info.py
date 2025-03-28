82360b784c7f0d477e09e5bed9008d2ed449698b
d3x-at <70750382+d3x-at@users.noreply.github.com>
2025-03-28 21:55:46 +0100

    remove typing from unused argument

--- src/sd_parsers/extractors/png_image_info.py
+++ src/sd_parsers/extractors/png_image_info.py
@@ -1,8 +1,7 @@
 from typing import Any, Dict
 from PIL import Image
-from sd_parsers.data import Generators
 
 
-def png_image_info(image: Image.Image, _: Generators) -> Dict[str, Any]:
+def png_image_info(image: Image.Image, _) -> Dict[str, Any]:
     """use image.info"""
     return image.info
--- src/sd_parsers/extractors/png_image_text.py
+++ src/sd_parsers/extractors/png_image_text.py
@@ -1,11 +1,10 @@
 from typing import Any, Dict, Optional
 from PIL import Image
 
-from sd_parsers.data import Generators
 from sd_parsers.exceptions import MetadataError
 
 
-def png_image_text(image: Image.Image, _: Generators) -> Optional[Dict[str, Any]]:
+def png_image_text(image: Image.Image, _) -> Optional[Dict[str, Any]]:
     """use PngImagePlugin.text property (iTxt, tEXt and zTXt chunks may appear at the end of the file)"""
     try:
         return image.text  # type: ignore
